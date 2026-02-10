4a9b3bb8efed18fbcd4344e1603971eacba3cda8
thomasguillet44 <guilletthomas59@gmail.com>
2026-02-10 19:40:36 +0100

    import police depuis index

--- src/frontend/App.vue
+++ src/frontend/App.vue
@@ -1,7 +1,4 @@
 <template>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Bitcount+Single:wght@100..900&display=swap" rel="stylesheet">
   <p>
     <Layout>
       <template #header>
